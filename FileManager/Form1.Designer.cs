cb3ae1cde986be131e3a352e9534cad553367540
oggy95 <gorobchuk95@gmail.com>
2017-06-11 11:19:26 +0300

    test commit

---
+++ FileManager/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.PauseButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.PositionBar = new System.Windows.Forms.TrackBar();
+            this.VolumeBar = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.PreviousBuuton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PositionBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VolumeBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Location = new System.Drawing.Point(143, 6);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(75, 23);
+            this.PlayButton.TabIndex = 1;
+            this.PlayButton.Text = "Play";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            this.PlayButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PauseButton
+            // 
+            this.PauseButton.Location = new System.Drawing.Point(224, 6);
+            this.PauseButton.Name = "PauseButton";
+            this.PauseButton.Size = new System.Drawing.Size(116, 23);
+            this.PauseButton.TabIndex = 2;
+            this.PauseButton.Text = "Pause/Play";
+            this.PauseButton.UseVisualStyleBackColor = true;
+            this.PauseButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(346, 6);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 3;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "File";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // PositionBar
+            // 
+            this.PositionBar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.PositionBar.Location = new System.Drawing.Point(465, 6);
+            this.PositionBar.Name = "PositionBar";
+            this.PositionBar.Size = new System.Drawing.Size(104, 45);
+            this.PositionBar.TabIndex = 10;
+            this.PositionBar.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // VolumeBar
+            // 
+            this.VolumeBar.Location = new System.Drawing.Point(358, 51);
+            this.VolumeBar.Name = "VolumeBar";
+            this.VolumeBar.Size = new System.Drawing.Size(104, 45);
+            this.VolumeBar.TabIndex = 11;
+            this.VolumeBar.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(385, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Volume";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.listBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(6, 35);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(346, 61);
+            this.listBox1.Sorted = true;
+            this.listBox1.TabIndex = 15;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // NextButton
+            // 
+            this.NextButton.Location = new System.Drawing.Point(427, 6);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(30, 23);
+            this.NextButton.TabIndex = 16;
+            this.NextButton.Text = ">>";
+            this.NextButton.UseVisualStyleBackColor = true;
+            this.NextButton.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // PreviousBuuton
+            // 
+            this.PreviousBuuton.Location = new System.Drawing.Point(108, 6);
+            this.PreviousBuuton.Name = "PreviousBuuton";
+            this.PreviousBuuton.Size = new System.Drawing.Size(29, 23);
+            this.PreviousBuuton.TabIndex = 17;
+            this.PreviousBuuton.Text = "<<";
+            this.PreviousBuuton.UseVisualStyleBackColor = true;
+            this.PreviousBuuton.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(472, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(472, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 19;
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(6, 117);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(75, 23);
+            this.LoadButton.TabIndex = 20;
+            this.LoadButton.Text = "Load Playlist";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(87, 116);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 21;
+            this.SaveButton.Text = "Save Playlist";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(169, 116);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 23);
+            this.ClearButton.TabIndex = 22;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(572, 149);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PreviousBuuton);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.VolumeBar);
+            this.Controls.Add(this.PositionBar);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.PauseButton);
+            this.Controls.Add(this.PlayButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+//            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PositionBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VolumeBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.Button PauseButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar PositionBar;
+        private System.Windows.Forms.TrackBar VolumeBar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.Button PreviousBuuton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button ClearButton;
+
+    }
+}
+
---
+++ FileManager/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+using WMPLib;
+
+
+namespace Player
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer wmp = new WindowsMediaPlayer();
+        private List<string[]> playList = new List<string[]>();
+        bool pausePlay;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)//пуск
+        {
+            try
+            {
+                wmp.controls.play();
+                PositionBar.Enabled = true;
+                VolumeBar.Enabled = true;
+                VolumeBar.Value = VolumeBar.Maximum;
+                timer1.Enabled = true;
+                timer1.Interval = 1000;
+                wmp.URL = playList[listBox1.SelectedIndex][1];
+                pausePlay = !pausePlay;
+                if (!pausePlay) wmp.controls.play();
+                this.Text = playList[listBox1.SelectedIndex][0];
+            }
+            catch (Exception ex)
+            {
+                if (playList.Count == 0)
+                {
+                    MessageBox.Show("Error!\n" + ex);
+                    button4_Click(sender,e);
+                    this.Close();
+                }
+
+                else
+                {
+                    listBox1.SelectedIndex = 0;
+                    wmp.URL = playList[0][1];
+                }
+            }
+        }
+
+        public void button2_Click(object sender, EventArgs e)//пауза/відновити
+        {        
+            if (!pausePlay)
+            {
+                wmp.controls.play();
+                PauseButton.Text = "Play";
+            }
+            else
+            {
+                wmp.controls.pause();
+                PauseButton.Text = "Pause";
+            }
+            pausePlay = !pausePlay;
+        }
+
+        public void button3_Click(object sender, EventArgs e)//стоп
+        {
+            wmp.controls.stop();
+        }
+
+        public void button4_Click(object sender, EventArgs e)//вибрати файл
+        {
+            var sourceFileOpenFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Filter = @"MP3 Files (*.mp3)|*.mp3",
+                RestoreDirectory = true,
+                Multiselect = true,
+                Title = @"Please select MP3 File(s) for opening"
+            };
+
+            if (sourceFileOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    foreach (string fileName in sourceFileOpenFileDialog.FileNames)
+                    {
+                        string shortPath = Path.GetFileName(fileName);
+                        shortPath = shortPath.Substring(0, shortPath.Length - 4);
+
+                        string[] pathElements = new string[2];
+                        pathElements[0] = shortPath;
+                        pathElements[1] = fileName;
+
+                        playList.Add(pathElements);
+                        listBox1.Items.Add(shortPath);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(@"Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }    
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+                PositionBar.Maximum = Convert.ToInt32(wmp.currentMedia.duration);
+                PositionBar.Value = Convert.ToInt32(wmp.controls.currentPosition);
+            
+            if (wmp != null)
+            {
+                int s = (int)wmp.currentMedia.duration;
+                int h = s / 3600;
+                int m = (s - (h * 3600)) / 60;
+                s = s - (h * 3600 + m * 60);
+                label3.Text = String.Format("{0:D}:{1:D2}:{2:D2}", h, m, s);
+
+                s = (int)wmp.controls.currentPosition;
+                h = s / 3600;
+                m = (s - (h * 3600)) / 60;
+                s = s - (h * 3600 + m * 60);
+                label2.Text = String.Format("{0:D}:{1:D2}:{2:D2}", h, m, s);
+            }
+            else
+            {
+                label2.Text = "0:00:00";
+                label3.Text = "0:00:00";
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            wmp.controls.currentPosition = PositionBar.Value;
+        }
+
+        public void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            wmp.settings.volume = VolumeBar.Value;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox1.SelectedIndex += 1;
+            }
+            catch (Exception) { listBox1.SelectedIndex = 0; }
+            //if (listBox1.SelectedIndex >= listBox1.Items.Count) listBox1.SelectedIndex = 0;
+            wmp.URL = playList[listBox1.SelectedIndex][1]; 
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex -= 1;
+            if (listBox1.SelectedIndex < 0) listBox1.SelectedIndex = playList.Count-1;                           
+            wmp.URL = playList[listBox1.SelectedIndex][1];
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            var sourceFileOpenFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Filter = @"Playlist Files (*.pls)|*.pls",
+                RestoreDirectory = true,
+                Multiselect = false,
+                Title = @"Please select playlist file for opening"
+            };
+
+            if (sourceFileOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string fileName = sourceFileOpenFileDialog.FileName;
+
+                    using (var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+                    {
+                        using (var sr = new StreamReader(fs, Encoding.GetEncoding(1251)))
+                        {
+                            playList.Clear();
+                            listBox1.Items.Clear();
+
+                            while (!sr.EndOfStream)
+                            {
+                                string[] pathElements = sr.ReadLine().Split('|');
+                                playList.Add(pathElements);
+                                listBox1.Items.Add(pathElements[0]);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(@"Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            var sourceFileSaveFileDialog = new SaveFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Filter = @"Playlist Files (*.pls)|*.pls",
+                RestoreDirectory = true,
+                Title = @"Please select playlist file for saving"
+            };
+
+            if (sourceFileSaveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (var fs = new FileStream(sourceFileSaveFileDialog.FileName, FileMode.Create, FileAccess.Write))
+                    {
+                        using (var sw = new StreamWriter(fs, Encoding.GetEncoding(1251)))
+                        {
+                            foreach (var song in playList)
+                            {
+                                string outputString = song[0] + "|" + song[1];
+                                sw.WriteLine(outputString);
+                            }
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(@"Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            playList.Clear();
+            listBox1.Items.Clear();
+        }
+
+    }
+}
